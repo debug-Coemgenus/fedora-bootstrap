973837fcdb0a02c9906e1177210380550f26cc06
Coemgenus <coemgenus-vc@pm.me>
2026-08-17 16:10:08 +0300

    niri: run add-wants as user

--- de/niri/configure.sh
+++ de/niri/configure.sh
@@ -11,13 +11,24 @@ log()  { printf '[configure:niri] %s\n' "$*"; }
 fail() { printf '[configure:niri] ERROR: %s\n' "$*" >&2; exit 1; }
 
 [[ $EUID -eq 0 ]] || fail "must be run as root"
+[[ -n "${SUDO_USER:-}" && "$SUDO_USER" != "root" ]] \
+    || fail "SUDO_USER not set - cannot configure the user's systemd session"
+
+TARGET_HOME="$(getent passwd "$SUDO_USER" | cut -d: -f6)" \
+    || fail "user not found: $SUDO_USER"
+TARGET_UID="$(id -u "$SUDO_USER")" \
+    || fail "could not determine UID for: $SUDO_USER"
 
 # --- SDDM (display manager) ---------------------------------------------------
 
 log "enabling sddm"
 systemctl enable --force sddm.service
 systemctl set-default graphical.target
-systemctl --user add-wants niri.service dms
+runuser -u "$SUDO_USER" -- env \
+    HOME="$TARGET_HOME" \
+    XDG_CONFIG_HOME="$TARGET_HOME/.config" \
+    XDG_RUNTIME_DIR="/run/user/$TARGET_UID" \
+    systemctl --user add-wants niri.service dms
 
 # --- niri portals configuration -----------------------------------------------
 
